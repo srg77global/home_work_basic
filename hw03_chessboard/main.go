c929f65af55b6dbae78e7e85c49147ed2accdbcd
Sergey Petrov <personal.sergeypetrov@gmail.com>
2024-09-01 21:24:31 +0300

    The hw3 is completed.

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size int
+
+	fmt.Printf("Enter the size for the chessboard:")
+
+	_, err := fmt.Scanf("%d", &size)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for row := 1; row <= size; row++ {
+		for cell := 1; cell <= size; cell++ {
+			switch {
+			case cell%2 == 0 && cell != size:
+				fmt.Print("#")
+			case cell%2 != 0 && cell != size:
+				fmt.Print(" ")
+			case cell%2 == 0 && cell == size:
+				fmt.Printf("#\n")
+			case cell%2 != 0 && cell == size:
+				fmt.Printf(" \n")
+			}
+		}
+	}
 }
