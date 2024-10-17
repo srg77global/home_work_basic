c966175304d5ebb80d36297cee49e7ae60ebeff1
Sergey <personal.sergeypetrov@gmail.com>
2024-10-17 20:05:27 +0300

    hw05 is corrected

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -6,38 +6,38 @@ import (
 )
 
 type Shape interface {
-	CalcArea() any
+	CalcArea() float64
 }
 
 type Circle struct {
-	R int
+	R float64
 }
 
-func (c *Circle) CalcArea() any {
-	return math.Pi * (float64(c.R) * float64(c.R))
+func (c *Circle) CalcArea() float64 {
+	return math.Pi * (c.R * c.R)
 }
 
 type Rectangle struct {
-	A, B int
+	A, B float64
 }
 
-func (r *Rectangle) CalcArea() any {
+func (r *Rectangle) CalcArea() float64 {
 	return r.A * r.B
 }
 
 type Triangle struct {
-	A, B int
+	A, B float64
 }
 
-func (t *Triangle) CalcArea() any {
+func (t *Triangle) CalcArea() float64 {
 	return t.A * t.B / 2
 }
 
-func calculateArea(s any) (any, error) {
+func calculateArea(s any) (float64, error) {
 	if ss, ok := s.(Shape); ok {
 		return ss.CalcArea(), nil
 	}
-	return "", fmt.Errorf("ошибка: переданный объект не является фигурой")
+	return 0, fmt.Errorf("ошибка: переданный объект не является фигурой")
 }
 
 func main() {
@@ -49,19 +49,19 @@ func main() {
 	if s1, err := calculateArea(a); err != nil {
 		fmt.Println("Ошибка: переданный объект не является фигурой.")
 	} else {
-		fmt.Printf("Круг: радиус %d\nПлощадь: %.14f\n", a.R, s1)
+		fmt.Printf("Круг: радиус %.0f\nПлощадь: %.14f\n", a.R, s1)
 	}
 
 	if s2, err := calculateArea(b); err != nil {
 		fmt.Println("Ошибка: переданный объект не является фигурой.")
 	} else {
-		fmt.Printf("Прямоугольник: ширина %d, высота %d\nПлощадь: %d\n", b.A, b.B, s2)
+		fmt.Printf("Прямоугольник: ширина %.0f, высота %.0f\nПлощадь: %.0f\n", b.A, b.B, s2)
 	}
 
 	if s3, err := calculateArea(c); err != nil {
 		fmt.Println("Ошибка: переданный объект не является фигурой.")
 	} else {
-		fmt.Printf("Треугольник: основание %d, высота %d\nПлощадь: %d\n", c.A, c.B, s3)
+		fmt.Printf("Треугольник: основание %.0f, высота %.0f\nПлощадь: %.0f\n", c.A, c.B, s3)
 	}
 
 	if s4, err := calculateArea(d); err != nil {
